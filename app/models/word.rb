96b4ec8c4daee253b54ede8dab848d4c56cd3593
namson168 <namsonqd@gmail.com>
2015-07-24 18:19:50 +0700

    Refs#34764 Show word list

--- app/models/word.rb
+++ app/models/word.rb
@@ -7,6 +7,15 @@ class Word < ActiveRecord::Base
   validates :content, presence: true, uniqueness: true
   validate :check_correct_answer
 
+  scope :search, ->search{where "content LIKE ?", "%#{search}%"}
+  scope :alphabet, ->{order "content ASC"}
+  scope :filter_category, ->category{where category_id: category if category.present?}
+  scope :learned, ->user{where "id IN (SELECT word_id FROM results WHERE lesson_id IN 
+                               (SELECT id FROM lessons WHERE user_id = ?))", user.id}
+  scope :not_learned, ->user{where "id NOT IN (SELECT word_id FROM results WHERE lesson_id IN 
+                                   (SELECT id FROM lessons WHERE user_id = ?))", user.id}
+  scope :get_all, ->user{}
+
   private
   def check_correct_answer
     errors.add :base, I18n.t("not_choice_correct") if answers.select{|opt| opt.correct}.blank?
